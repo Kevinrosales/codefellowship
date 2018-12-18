6a535a524f4eea10ab772acc50fd8b73b218bf88
Kevinrosales <kevinrosalesolvera@gmail.com>
2018-12-18 09:26:09 -0800

    inital commit

---
+++ src/main/java/com/kevinrosales/lab16/Lab16Application.java
@@ -0,0 +1,14 @@
+package com.kevinrosales.lab16;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab16Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab16Application.class, args);
+	}
+
+}
+
